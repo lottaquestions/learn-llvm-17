f5c198cd9ec8f8f06bb5d040957b571acabc2072
lottaquestions <nickothieno@yahoo.com>
2025-04-10 21:03:31 -0400

    Adding error messages and completing tokens for the lexer

--- Chapter3_TurningSourceIntoAST/tinylang/include/tinylang/Basic/TokenKinds.h
+++ Chapter3_TurningSourceIntoAST/tinylang/include/tinylang/Basic/TokenKinds.h
@@ -1,5 +1,6 @@
 #ifndef TINYLANG_BASIC_TOKENKINDS_H
 #define TINYLANG_BASIC_TOKENKINDS_H
+#include "llvm/Support/Compiler.h"
 namespace tinylang{
     namespace tok{
         enum TokenKind : unsigned short {
@@ -7,9 +8,9 @@ namespace tinylang{
             #include "TokenKinds.def"
             NUM_TOKENS
         };
-        const char *getTokenName(TokenKind Kind);
-        const char *getPunctuatorSpelling(TokenKind Kind);
-        const char *getKeywordSpelling(TokenKind Kind);
+        const char *getTokenName(TokenKind Kind) LLVM_READNONE;
+        const char *getPunctuatorSpelling(TokenKind Kind) LLVM_READNONE;
+        const char *getKeywordSpelling(TokenKind Kind) LLVM_READNONE;
     } // namespace tok
 } // namespace tinylang
 
